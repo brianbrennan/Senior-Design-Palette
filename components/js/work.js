82932c243e8d7a1c6383511158a6600312bc00f9
Brian Brennan <webintern@cardsandpockets.com>
2015-04-02 12:44:18 -0400

    Added List of Students

---
+++ components/js/work.js
@@ -0,0 +1,31 @@
+var work = [{
+	'name' : "Alex Ordway"	
+},{
+	'name' : "Alexandra Goveia"	
+},{
+	'name' : "Alphonse Riang"	
+},{
+	'name' : "Andrew Ahern"	
+},{
+	'name' : "Angie Belculfine"	
+},{
+	'name' : "Brian Brennan"	
+},{
+	'name' : "Colleen D'Alessandro"	
+},{
+	'name' : "Jaron Cote"	
+},{
+	'name' : "Jaysen Odom"	
+},{
+	'name' : "Jordan Harris"	
+},{
+	'name' : "Marissa Maccioli"	
+},{
+	'name' : "Mary Heelan"	
+},{
+	'name' : "Megan Knapp"	
+},{
+	'name' : "Tori Faieta"	
+},{
+	'name' : "Tricia Belden"	
+}];
